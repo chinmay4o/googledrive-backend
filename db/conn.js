f0b6aaaa0e304b2d3074191d64d8f47b530376c6
surve4404 <surve4404@gmail.com>
2021-07-06 12:50:29 +0530

    added post req

---
+++ db/conn.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+//mongoose
+const url = process.env.DATABASE;
+
+mongoose.connect(url, { 
+    useNewUrlParser: true ,
+    useCreateIndex: true,
+    useUnifiedTopology: true ,
+    useFindAndModify: false
+})
+const conn = mongoose.connection;
+conn.on("open" , () => console.log("Mongodb connected"));
--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import {userRouter} from "./routes/user.js";
+import { userRouter } from "./routes/user.js";
 import cors from "cors";
+// import "./db/conn.js";
 
-dotenv.config({path : "./config.env"});
+dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 5003;
-const url = process.env.DATABASE;
-
 
 const app = express();
+//mongoose connect
+const url = process.env.DATABASE;
 
-//mongoose
-mongoose.connect(url, { 
-    useNewUrlParser: true ,
-    useCreateIndex: true,
-    useUnifiedTopology: true ,
-    useFindAndModify: false
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 const conn = mongoose.connection;
-conn.on("open" , () => console.log("Mongodb connected"));
+conn.on("open", () => console.log("Mongodb connected"));
 
-const middleware = (req, res , next) => {
-
-}
-app.listen(PORT , () => console.log("connected to port 5003"));
+// const middleware = (req, res, next) => {};
+// app.listen(PORT, () => console.log("connected to port 5003"));
 
 //middler ware
 app.use(cors());
 app.use(express.json());
-
